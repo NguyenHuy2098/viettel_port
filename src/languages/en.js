433210326697060dcc7c4da1128404f4c9f49281
buivanhuu2017 <buivanhuu2017@gmail.com>
2019-06-13 09:49:00 +0700

    feat: 🎸 Create manifest page

--- src/languages/en.js
+++ src/languages/en.js
@@ -1,6 +1,21 @@
 const en = {
   translation: {
     'Welcome to React': 'Welcome to React and react-i18next',
+    'Tạo bảng kê nội tỉnh': 'Tạo bảng kê nội tỉnh',
+    'Bưu cục đến': 'Bưu cục đến',
+    'Ghi chú': 'Ghi chú',
+    'Ghi lại': 'Ghi lại',
+    'Nhập ghi chú': 'Nhập ghi chú',
+    'Đóng bảng kê': 'Đóng bảng kê',
+    'Tìm kiếm bảng kê': 'Tìm kiếm bảng kê',
+    'Tạo bảng kê': 'Tạo bảng kê',
+    'Tổng số': 'Tổng số',
+    'Mã bảng kê': 'Mã bảng kê',
+    'Bưu cục đi': 'Bưu cục đi',
+    'Số lượng': 'Số lượng',
+    'Người nhập': 'Người nhập',
+    'Ngày nhập': 'Ngày nhập',
+    'Quản trị': 'Quản trị',
   },
 };
 
