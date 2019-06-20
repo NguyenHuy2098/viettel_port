fd97c95f2de40d6a5f9cbd602df5b1c75b4d89c3
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2019-06-20 15:54:43 +0700

    feat: 🎸 Add Switcher

---
+++ src/components/Switcher/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Redirect, Route, RouteComponentProps, Switch } from 'react-router';
+import { Helmet } from 'react-helmet';
+import { SIPRoutePropsType } from 'layouts/types';
+
+interface Props {
+  routes: SIPRoutePropsType[];
+}
+
+const Switcher: React.FC<Props> = (props: Props): JSX.Element => {
+  return (
+    <Switch>
+      {props.routes.map(
+        (route: SIPRoutePropsType, idx: number): React.ReactNode => {
+          const { component: Component } = route;
+          /* eslint-disable react/jsx-no-bind */
+          return (
+            Component && (
+              <Route
+                key={idx}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps): JSX.Element => (
+                  <>
+                    <Helmet>
+                      <title>{route.name}</title>
+                    </Helmet>
+                    <Component {...props} />
+                  </>
+                )}
+              />
+            )
+          );
+          /* eslint-enable react/jsx-no-bind */
+        },
+      )}
+      <Redirect to="/not-found" />
+    </Switch>
+  );
+};
+
+export default Switcher;
