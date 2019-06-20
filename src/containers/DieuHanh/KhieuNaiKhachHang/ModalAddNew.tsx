a2f46a8abd4d5ffc7f304fc8e73090ab963ec86c
Tuyền Vinc <vinctuyen2694@gmail.com>
2019-06-20 10:33:17 +0700

    fix: 🐛 fix UI popup

--- src/containers/DieuHanh/KhieuNaiKhachHang/ModalAddNew.tsx
+++ src/containers/DieuHanh/KhieuNaiKhachHang/ModalAddNew.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Modal, ModalHeader, ModalBody, Row, Table } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Label, Col, Input } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 
 interface Props {
@@ -14,70 +14,79 @@ const ModalAddNew: React.FC<Props> = (props): JSX.Element => {
   // eslint-disable-next-line max-lines-per-function
   function renderInformationTable(): JSX.Element {
     return (
-      <div className="sipTableContainer">
+      <div className="border-bottom mb-3">
         <Row className="mb-3">
-          <h6>{t('Thông tin biên bản')}</h6>
+          <Label xs="12" lg="4">
+            {t('Tên khách hàng/công ty')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
         </Row>
-        <Table bordered hover className="mb-3">
-          <thead className="text-center">
-            <tr>
-              <th>{t('Mã lỗi')}</th>
-              <th>{t('Ghi chú')}</th>
-              <th>{t('Điểm phạt cá nhân')}</th>
-              <th>{t('Điểm phạt GD')}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{t('1007')}</td>
-              <td>{t('Phát bưu gửi chậm thời gian quy định')}</td>
-              <td>{t('10')}</td>
-              <td>{t('5')}</td>
-            </tr>
-          </tbody>
-        </Table>
         <Row className="mb-3">
-          <h6>{t('Danh sách phiếu gửi của biên bản')}</h6>
+          <Label xs="12" lg="4">
+            {t('Điện thoại')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
         </Row>
-        <Table bordered hover className="mb-3">
-          <thead className="text-center">
-            <tr>
-              <th>{t('Mã vận đơn')}</th>
-              <th>{t('Mã chuyến thư')}</th>
-              <th>{t('Ghi chú')}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{t('515324586')}</td>
-              <td>{t('CT_371029_TTHNI')}</td>
-              <td>{t('Bưu phẩm được phân công ngày 11/04 chưa đi phát')}</td>
-            </tr>
-          </tbody>
-        </Table>
         <Row className="mb-3">
-          <h6>{t('Kết luận biên bản')}</h6>
+          <Label xs="12" lg="4">
+            {t('Địa chỉ')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
         </Row>
-        <Table bordered hover className="mb-3">
-          <thead className="text-center">
-            <tr>
-              <th>{t('Ngày kết luận')}</th>
-              <th>{t('Bưu cục kết luận')}</th>
-              <th>{t('Người kết luận')}</th>
-              <th>{t('Người phạm lỗi')}</th>
-              <th>{t('Ghi chú')}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{t('14/04/2019')}</td>
-              <td>{t('KAH')}</td>
-              <td>{t('Nguyễn Mạnh Quang')}</td>
-              <td>{t('Đỗ Minh Tú')}</td>
-              <td>{t('Bưu phẩm được phân công ngày 11/04 chưa đi phát')}</td>
-            </tr>
-          </tbody>
-        </Table>
+        <Row className="mb-3">
+          <Label xs="12" lg="4">
+            {t('Mã phiếu gửi')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Label xs="12" lg="4">
+            {t('Loại khiếu nại')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Label xs="12" lg="4">
+            {t('Chọn khiếu nại')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Label xs="12" lg="4">
+            {t('Mức độ')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Label xs="12" lg="4">
+            {t('Nội dung')}
+          </Label>
+          <Col lg="8">
+            <Input type="textarea" />
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Label xs="12" lg="4">
+            {t('Bưu cục xử lý')}
+          </Label>
+          <Col lg="8">
+            <Input type="text" />
+          </Col>
+        </Row>
       </div>
     );
   }
@@ -90,6 +99,14 @@ const ModalAddNew: React.FC<Props> = (props): JSX.Element => {
             {t('Tra cứu biên bản')}
           </ModalHeader>
           <ModalBody>{renderInformationTable()}</ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={props.toggle}>
+              {t('GHI LẠI')}
+            </Button>
+            <Button color="primary" onClick={props.toggle}>
+              {t('THOÁT')}
+            </Button>
+          </ModalFooter>
         </Modal>
       </div>
     );
