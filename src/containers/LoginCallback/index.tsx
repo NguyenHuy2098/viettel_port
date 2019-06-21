0b7470f30de8e1e6136092375be7650eb6789a1a
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2019-06-21 11:48:08 +0700

    fix: 🐛 Login callback with new URL

--- src/containers/LoginCallback/index.tsx
+++ src/containers/LoginCallback/index.tsx
@@ -12,17 +12,21 @@ const LoginCallback: React.FC = (): JSX.Element => {
     dispatch(push('/'));
   };
 
+  const successCallback = (): void => {
+    redirectToPreviousLocation();
+  };
+
+  const errorCallback = (): void => {
+    redirectToPreviousLocation();
+  };
+
   return (
     <>
       <Helmet>
         <meta name="description" content="Logging in by SSO" />
       </Helmet>
 
-      <CallbackComponent
-        userManager={userManager}
-        successCallback={redirectToPreviousLocation}
-        errorCallback={redirectToPreviousLocation}
-      >
+      <CallbackComponent userManager={userManager} successCallback={successCallback} errorCallback={errorCallback}>
         <div>Redirecting...</div>
       </CallbackComponent>
     </>
