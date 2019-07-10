741f99cfab104a9dc6d0038a0ac20b51b864e416
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2019-07-10 15:39:33 +0700

    refactor: 💡 Reduce scss from five columns row

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -8,44 +8,45 @@ const Home: React.FC = (): JSX.Element => {
   function renderTopDashBoard(): JSX.Element {
     return (
       <Row className="topDashBroad mb-4">
-        <Col xs="12" className=" col-sm itemCol">
-          <span className="background-color-primary">
-            <i className="fa fa-file-text-o fa-lg mt-4 icon-1" />
+        <Col xs="12" className="col-sm itemCol pt-3">
+          <span className="font-lg">
+            <i className="fa fa-file-text-o fa-lg color-red mr-2" />
             Bảng kê chưa đóng
           </span>
-          <div className="numCol">20</div>
+          <div className="font-2xl color-gray-dark pt-2 pb-2">20</div>
         </Col>
-        <Col xs="12" className=" col-sm itemCol">
-          <span>
-            <i className="fa fa-refresh fa-lg mt-4 icon-2" />
+        <Col xs="12" className="col-sm itemCol pt-3">
+          <span className="font-lg">
+            <i className="fa fa-refresh fa-lg color-violet mr-2" />
             Tải chưa đóng
           </span>
-          <div className="numCol">02</div>
+          <div className="font-2xl color-gray-dark pt-2 pb-2">02</div>
         </Col>
-        <Col xs="12" className=" col-sm itemCol">
-          <span>
-            <i className="fa fa-bus fa-lg mt-4 icon-3" />
+        <Col xs="12" className="col-sm itemCol pt-3">
+          <span className="font-lg">
+            <i className="fa fa-bus fa-lg color-bluegreen mr-2" />
             Chuyến thư chưa đóng
           </span>
-          <div className="numCol">01</div>
+          <div className="font-2xl color-gray-dark pt-2 pb-2">01</div>
         </Col>
-        <Col xs="12" className=" col-sm itemCol">
-          <span>
-            <i className="fa fa-file-text-o fa-lg mt-4 icon-4" />
+        <Col xs="12" className="col-sm itemCol pt-3">
+          <span className="font-lg">
+            <i className="fa fa-file-text-o fa-lg color-blue mr-2" />
             Bảng kê chưa nhận
           </span>
-          <div className="numCol">20</div>
+          <div className="font-2xl color-gray-dark pt-2 pb-2">20</div>
         </Col>
-        <Col xs="12" className=" col-sm itemCol">
-          <span>
-            <i className="fa fa-refresh fa-lg mt-4 icon-5" />
+        <Col xs="12" className="col-sm itemCol pt-3">
+          <span className="font-lg">
+            <i className="fa fa-refresh fa-lg color-orange mr-2" />
             Tải chưa nhận
           </span>
-          <div className="numCol">20</div>
+          <div className="font-2xl color-gray-dark pt-2 pb-2">20</div>
         </Col>
       </Row>
     );
   }
+
   return (
     <>
       {renderTopDashBoard()}
