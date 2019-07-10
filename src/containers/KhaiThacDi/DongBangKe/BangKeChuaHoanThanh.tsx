99c53d13df0f11d94c1165ccaa0b89c0e362c201
huubv <huubv@gmail.com>
2019-07-10 13:54:22 +0700

    feat: 🎸 Update package

---
+++ src/containers/KhaiThacDi/DongBangKe/BangKeChuaHoanThanh.tsx
@@ -0,0 +1,186 @@
+import * as React from 'react';
+import {
+  Button,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Row,
+  Table,
+} from 'reactstrap';
+import { map } from 'lodash';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { action_MIOA_ZTMI047 } from 'redux/MIOA_ZTMI047/actions';
+import { useGet_MT_ZTMI047_OUT_Row } from 'redux/MIOA_ZTMI047/selectors';
+import { HttpRequestErrorType } from 'utils/HttpRequetsError';
+
+// eslint-disable-next-line max-lines-per-function
+const BangKeChuaHoanThanh: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+  const [modalCreateNew, setModalCreateNew] = React.useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  React.useEffect((): void => {
+    const payload = {
+      IV_TOR_ID: '',
+      IV_TOR_TYPE: 'ZC1',
+      IV_FR_LOC_ID: 'BDH',
+      IV_CUST_STATUS: '101',
+      IV_TO_LOC_ID: '',
+    };
+    dispatch(
+      action_MIOA_ZTMI047(payload, {
+        onFailure: (error: HttpRequestErrorType): void => {
+          console.log(error.messages);
+        },
+      }),
+    );
+  }, [dispatch]);
+
+  function toggle(): void {
+    setModalCreateNew(!modalCreateNew);
+  }
+
+  function renderModal(): JSX.Element {
+    return (
+      <Modal isOpen={modalCreateNew} toggle={toggle} className="sipTitleModalCreateNew">
+        <ModalHeader toggle={toggle}>{t('Tạo bảng kê')}</ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Label>{t('Bưu cục đến')}</Label>
+            <Input type="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label>{t('Ghi chú')}</Label>
+            <Input type="textarea" placeholder={t('Nhập ghi chú')} />
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={toggle}>{t('Ghi lại')}</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+
+  function renderPagination(): JSX.Element {
+    return (
+      <Pagination className="sipPagination">
+        <PaginationItem className="sipPaginationPrev pull-left">
+          <PaginationLink previous href="#">
+            <i className="fa fa-arrow-left"></i>
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem active>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">4</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">5</PaginationLink>
+        </PaginationItem>
+        <PaginationItem className="sipPaginationNext pull-right">
+          <PaginationLink next href="#">
+            <i className="fa fa-arrow-right"></i>
+          </PaginationLink>
+        </PaginationItem>
+      </Pagination>
+    );
+  }
+
+  function renderAction(): JSX.Element {
+    return (
+      <>
+        <Button>
+          <i className="fa fa-print fa-lg color-green" />
+        </Button>
+        <Button>
+          <i className="fa fa-pencil fa-lg color-blue" />
+        </Button>
+        <Button>
+          <i className="fa fa-trash-o fa-lg color-red" />
+        </Button>
+      </>
+    );
+  }
+
+  const listManifest = useGet_MT_ZTMI047_OUT_Row();
+
+  return (
+    <>
+      <Row className="mb-3 sipTitleContainer">
+        <h1 className="sipTitle">{t('Đóng bảng kê')}</h1>
+        <div className="sipTitleRightBlock">
+          <div className="sipTitleRightBlockInput">
+            <i className="fa fa-search" />
+            <Input type="text" placeholder={t('Tìm kiếm bảng kê')} />
+          </div>
+          <Button onClick={toggle}>
+            <i className="fa fa-plus" />
+            {t('Tạo bảng kê')}
+          </Button>
+          {renderModal()}
+        </div>
+      </Row>
+      <p className="text-right">
+        {t('Tổng số')}: <span>56</span>
+      </p>
+      <div className="mt-3" />
+      <Row className="sipTableContainer">
+        <Table striped hover>
+          <thead>
+            <tr>
+              <th>{t('Mã bảng kê')}</th>
+              <th>{t('Bưu cục đi')}</th>
+              <th>{t('Bưu cục đến')}</th>
+              <th>{t('Số lượng')}</th>
+              <th>{t('Người nhập')}</th>
+              <th>{t('Ngày nhập')}</th>
+              <th>{t('Ghi chú')}</th>
+              <th>{t('Quản trị')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {map(
+              listManifest,
+              (item, index): JSX.Element => {
+                return (
+                  <tr key={index}>
+                    <td>BK-2683077-TTKT1</td>
+                    <td>TTKT1</td>
+                    <td>TTKT3</td>
+                    <td>25</td>
+                    <td>Nguyễn Văn An</td>
+                    <td>19/6/2019</td>
+                    <td>Hàng giá trị cao</td>
+                    <td className="SipTableFunctionIcon">{renderAction()}</td>
+                  </tr>
+                );
+              },
+            )}
+          </tbody>
+        </Table>
+        {renderPagination()}
+      </Row>
+    </>
+  );
+};
+
+export default BangKeChuaHoanThanh;
