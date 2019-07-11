b34cb3793271e51f78de67ec4d34e73d53068e6f
Nguyen Thu Thuy <thuthuy.nguyen1509@gmail.com>
2019-07-11 10:52:38 +0700

    feat: 🎸 Update api dong chuyen thu

---
+++ src/containers/KhaiThacDi/DongChuyenThu/ChuyenThuChuaHoanThanh.tsx
@@ -0,0 +1,207 @@
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { map } from 'lodash';
+import { push } from 'connected-react-router';
+import { Button, Col, Input, Label, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
+import { action_MIOA_ZTMI047 } from 'redux/MIOA_ZTMI047/actions';
+import { HttpRequestErrorType } from 'utils/HttpRequetsError';
+import { useGet_MT_ZTMI047_OUT_Row } from 'redux/MIOA_ZTMI047/selectors';
+import routesMap from 'utils/routesMap';
+import { action_MIOA_ZTMI016 } from 'redux/MIOA_ZTMI016/actions';
+
+// eslint-disable-next-line max-lines-per-function
+const ChuyenThuChuaHoanThanh: React.FC = (): JSX.Element => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    const payload = {
+      IV_TOR_ID: '',
+      IV_TOR_TYPE: 'ZC3',
+      IV_FR_LOC_ID: 'BDH',
+      IV_CUST_STATUS: '101',
+      IV_TO_LOC_ID: '',
+    };
+    dispatch(
+      action_MIOA_ZTMI047(payload, {
+        onFailure: (error: HttpRequestErrorType): void => {
+          console.log(error.messages);
+        },
+      }),
+    );
+  }, [dispatch]);
+
+  const listManifest = useGet_MT_ZTMI047_OUT_Row();
+  console.log(listManifest);
+
+  function renderPagination(): JSX.Element {
+    return (
+      <Pagination className="sipPagination">
+        <PaginationItem className="sipPaginationPrev pull-left">
+          <PaginationLink previous href="#">
+            <i className="fa fa-arrow-left" />
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem active>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">4</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">5</PaginationLink>
+        </PaginationItem>
+        <PaginationItem className="sipPaginationNext pull-right">
+          <PaginationLink next href="#">
+            <i className="fa fa-arrow-right" />
+          </PaginationLink>
+        </PaginationItem>
+      </Pagination>
+    );
+  }
+
+  // function renderAction(): JSX.Element {
+  //   return (
+  //     <>
+  //       <Button>
+  //         <i className="fa fa-print fa-lg color-green" />
+  //       </Button>
+  //       <Button>
+  //         <i className="fa fa-pencil fa-lg color-blue" />
+  //       </Button>
+  //       <Button>
+  //         <i className="fa fa-trash-o fa-lg color-red" />
+  //       </Button>
+  //     </>
+  //   );
+  // }
+
+  const handleRedirectDetail = (item: API.RowMTZTMI047OUT): ((event: React.MouseEvent) => void) => {
+    return (): void => {
+      dispatch(push(`${routesMap.danhSachPhieuGuiTrongBangKe}/${item.TOR_ID}`));
+    };
+  };
+
+  const handleDeleteManifet = (item: API.RowMTZTMI047OUT): ((event: React.MouseEvent) => void) => {
+    return (): void => {
+      const payload = {
+        IV_FLAG: '3',
+        IV_TOR_TYPE: 'ZC1',
+        IV_TOR_ID_CU: item.TOR_ID,
+        IV_SLOCATION: '',
+        IV_DLOCATION: '',
+        IV_DESCRIPTION: '',
+        T_ITEM: {
+          ITEM_ID: '',
+          ITEM_TYPE: '',
+        },
+      };
+      if (!window.confirm('Bạn có chắc chắn?')) return;
+      dispatch(
+        action_MIOA_ZTMI016(payload, {
+          onFinish: (): void => {
+            const payload = {
+              IV_TOR_ID: '',
+              IV_TOR_TYPE: 'ZC1',
+              IV_FR_LOC_ID: 'BDH',
+              IV_CUST_STATUS: '101',
+              IV_TO_LOC_ID: '',
+            };
+            dispatch(action_MIOA_ZTMI047(payload));
+          },
+        }),
+      );
+    };
+  };
+
+  function renderManifestTableAction(item: API.RowMTZTMI047OUT): JSX.Element {
+    return (
+      <>
+        <Button>
+          <i className="fa fa-print fa-lg color-green" />
+        </Button>
+        <Button onClick={handleRedirectDetail(item)}>
+          <i className="fa fa-pencil fa-lg color-blue" />
+        </Button>
+        <Button onClick={handleDeleteManifet(item)}>
+          <i className="fa fa-trash-o fa-lg color-red" />
+        </Button>
+      </>
+    );
+  }
+
+  function renderTable1(): JSX.Element {
+    return (
+      <>
+        <Table striped hover>
+          <thead>
+            <tr>
+              <th />
+              <th>{t('Mã chuyến thư')}</th>
+              <th>{t('Điểm đến')}</th>
+              <th>{t('SL')}</th>
+              <th>{t('Người nhập')}</th>
+              <th>{t('Ngày nhập')}</th>
+              <th>{t('Ghi chú')}</th>
+              <th>{t('Quản trị')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {map(
+              listManifest,
+              (item: API.RowMTZTMI047OUT, index): JSX.Element => {
+                return (
+                  <tr key={index}>
+                    <td className="text-center">
+                      <Label check>
+                        {/* eslint-disable-next-line react/jsx-max-depth */}
+                        <Input type="checkbox" />
+                      </Label>
+                    </td>
+                    <td>{item.TOR_ID}</td>
+                    <td></td>
+                    <td>{item.ITEM_NO}</td>
+                    <td></td>
+                    <td>{item.DATETIME_CHLC}</td>
+                    <td>{item.EXEC_CONT}</td>
+                    <td className="SipTableFunctionIcon">{renderManifestTableAction(item)}</td>
+                  </tr>
+                );
+              },
+            )}
+          </tbody>
+        </Table>
+        {renderPagination()}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Row className="sipContentContainer">
+        <Col lg={4} xs={12} className="p-0">
+          <div className="sipTitleRightBlockInput m-0">
+            <i className="fa fa-search" />
+            <Input type="text" placeholder={t('Tìm kiếm chuyến thư')} />
+          </div>
+        </Col>
+        <Col>
+          <p className="text-right mt-2 mb-0">
+            {t('Tổng số')}: <span>56</span>
+          </p>
+        </Col>
+      </Row>
+      <div className="mt-3" />
+      <Row className="sipTableContainer">{renderTable1()}</Row>
+    </>
+  );
+};
+
+export default ChuyenThuChuaHoanThanh;
