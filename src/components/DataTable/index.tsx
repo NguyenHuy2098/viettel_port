5305c84fcb04b3fd1c1b08c053e61f59cae71a05
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2019-09-20 17:22:37 +0700

    fix: 🐛 Add no-data status

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback } from 'react';
-import { Table } from 'reactstrap';
+import { Container, Row, Table } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
 import { TableOptions, useTable } from 'react-table';
-import { noop } from 'lodash';
+import { isEmpty, noop } from 'lodash';
+import noData from './no-data.svg';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 interface Props extends TableOptions<any> {
@@ -13,6 +15,8 @@ interface Props extends TableOptions<any> {
 
 // eslint-disable-next-line max-lines-per-function
 const DataTable: React.FC<Props> = ({ columns, data, onRowClick }: Props): JSX.Element => {
+  const { t } = useTranslation();
+
   // Use the state and functions returned from useTable to build your UI
   const { getTableProps, headerGroups, rows, prepareRow } = useTable({
     columns,
@@ -30,35 +34,47 @@ const DataTable: React.FC<Props> = ({ columns, data, onRowClick }: Props): JSX.E
 
   // Render the UI for your table
   return (
-    <Table striped hover {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup, index) => (
-          <tr key={index}>
-            {headerGroup.headers.map((column, index) => (
-              <th key={index} {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {rows.map(
-          (row, index) =>
-            prepareRow(row) || (
-              <tr key={index} {...row.getRowProps()}>
-                {row.cells.map((cell, index) => {
-                  return (
-                    <td key={index} onClick={handleClickRow(row.original)} {...cell.getCellProps()}>
-                      {cell.render('Cell')}
-                    </td>
-                  );
-                })}
-              </tr>
-            ),
-        )}
-      </tbody>
-    </Table>
+    <>
+      <Table striped hover {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup, index) => (
+            <tr key={index}>
+              {headerGroup.headers.map((column, index) => (
+                <th key={index} {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {rows.map(
+            (row, index) =>
+              prepareRow(row) || (
+                <tr key={index} {...row.getRowProps()}>
+                  {row.cells.map((cell, index) => {
+                    return (
+                      <td key={index} onClick={handleClickRow(row.original)} {...cell.getCellProps()}>
+                        {cell.render('Cell')}
+                      </td>
+                    );
+                  })}
+                </tr>
+              ),
+          )}
+        </tbody>
+      </Table>
+      {isEmpty(rows) && (
+        <Container fluid>
+          <Row className="align-items-center flex-column mb-3">
+            <img alt="no-data" className="w-auto" src={noData} />
+          </Row>
+          <Row className="align-items-center flex-column">
+            <span className="text-bold text-uppercase">{t('Không tìm thấy dữ liệu')}</span>
+          </Row>
+        </Container>
+      )}
+    </>
   );
 };
 
