683170d881213c8a0a45f0cf84f5bcbbd6e34e57
longlv <longlv0208@gmail.com>
2019-09-24 10:44:55 +0700

    fix: 🐛 tra cuu tai kien input redirect to ThongTinTaiKien

--- src/containers/KhaiThacDen/NhanTaiKien/index.tsx
+++ src/containers/KhaiThacDen/NhanTaiKien/index.tsx
@@ -1,14 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { KeyboardEvent, useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, TabContent, TabPane, Nav, NavItem, NavLink, Badge, Input } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 import classNames from 'classnames';
+import { push } from 'connected-react-router';
 import moment from 'moment';
+import { generatePath } from 'react-router';
+import { size, trim } from 'lodash';
 
 import { makeSelectorMaBP } from 'redux/auth/selectors';
 import { action_MIOA_ZTMI047 } from 'redux/MIOA_ZTMI047/actions';
 import { makeSelectorCountChuyenThuChuaNhanTaiKien, makeSelectorRowSize } from 'redux/MIOA_ZTMI047/selectors';
 import { SipDataState, SipDataType } from 'utils/enums';
+import routesMap from 'utils/routesMap';
 import ChuyenThuChuaNhanTaiKien from './ChuyenThuChuaNhanTaiKien';
 import TaiDaNhan from './TaiDaNhan';
 import NhanRiengTaiKien from './NhanRiengTaiKien';
@@ -31,7 +35,9 @@ const NhanTaiKien: React.FC = (): JSX.Element => {
         IV_FR_LOC_ID: '',
         IV_TO_LOC_ID: maBP,
         IV_CUST_STATUS: SipDataState.CHUYEN_THU_DA_QUET_NHAN,
-        IV_FR_DATE: moment().subtract(7),
+        IV_FR_DATE: moment()
+          .subtract(7, 'day')
+          .format('YYYYMMDD'),
         IV_TO_DATE: moment().format('YYYYMMDD'),
         IV_PAGENO: IV_PAGENO,
         IV_NO_PER_PAGE: '5000',
@@ -47,7 +53,9 @@ const NhanTaiKien: React.FC = (): JSX.Element => {
         IV_FR_LOC_ID: '',
         IV_TO_LOC_ID: maBP,
         IV_CUST_STATUS: SipDataState.TAI_KIEN_DA_QUET_NHAN,
-        IV_FR_DATE: '20100917',
+        IV_FR_DATE: moment()
+          .subtract(7, 'day')
+          .format('YYYYMMDD'),
         IV_TO_DATE: moment().format('YYYYMMDD'),
         IV_PAGENO: IV_PAGENO,
         IV_NO_PER_PAGE: '10',
@@ -63,7 +71,9 @@ const NhanTaiKien: React.FC = (): JSX.Element => {
         IV_FR_LOC_ID: '',
         IV_TO_LOC_ID: maBP,
         IV_CUST_STATUS: SipDataState.CHUYEN_THU_DA_QUET_NHAN,
-        IV_FR_DATE: '20100917',
+        IV_FR_DATE: moment()
+          .subtract(7, 'day')
+          .format('YYYYMMDD'),
         IV_TO_DATE: moment().format('YYYYMMDD'),
         IV_PAGENO: IV_PAGENO,
         IV_NO_PER_PAGE: '10',
@@ -82,6 +92,13 @@ const NhanTaiKien: React.FC = (): JSX.Element => {
     setTab(tab);
   }
 
+  function handleTraCuuTaiKien(e: KeyboardEvent<HTMLInputElement>): void {
+    const thisValue = e.currentTarget.value;
+    if (size(trim(thisValue)) && e.keyCode === 13) {
+      dispatch(push(generatePath(routesMap.THONG_TIN_TAI, { idTaiKien: thisValue })));
+    }
+  }
+
   return (
     <>
       <div className="row mt-3" />
@@ -89,7 +106,7 @@ const NhanTaiKien: React.FC = (): JSX.Element => {
         <h1 className="sipTitle">{t('Nhận tải kiện')}</h1>
         <div className="sipTitleRightBlockInput m-0">
           <i className="fa fa-search" />
-          <Input type="text" placeholder={t('Tra cứu tải/kiện')} />
+          <Input type="text" placeholder={t('Tra cứu tải/kiện')} onKeyUp={handleTraCuuTaiKien} />
         </div>
       </Row>
       <div className="row mt-3" />
