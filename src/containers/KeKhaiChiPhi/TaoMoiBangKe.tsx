1d037f5827107bf0b4a62569b10c8aa855cd3784
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2019-10-18 15:15:16 +0700

    fix: 🐛 Update boilerplate TaoMoiBangKe

--- src/containers/KeKhaiChiPhi/TaoMoiBangKe.tsx
+++ src/containers/KeKhaiChiPhi/TaoMoiBangKe.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Input, Row, Label } from 'reactstrap';
+import { Button, Col, FormGroup, Input, Label, Row } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import { match } from 'react-router-dom';
 import { Cell } from 'react-table';
@@ -16,21 +16,6 @@ interface Props {
 const TaoMoiBangKe = (props: Props): JSX.Element => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-
-  const renderTopController = (): JSX.Element => {
-    return (
-      <>
-        <Button color="primary" className="ml-2">
-          <img src={'../../assets/img/icon/iconPrint.svg'} alt="VTPostek" />
-          {t('In bảng kê')}
-        </Button>
-        <Button color="primary" className="ml-2">
-          <img src={'../../assets/img/icon/iconPlus.svg'} alt="VTPostek" />
-          {t('Thêm mới')}
-        </Button>
-      </>
-    );
-  };
 
   const columns = useMemo(
     //eslint-disable-next-line max-lines-per-function
@@ -90,27 +75,116 @@ const TaoMoiBangKe = (props: Props): JSX.Element => {
     dispatch(goBack());
   };
 
+  const renderFirstControllers = (): JSX.Element => (
+    <>
+      <Button color="primary" className="ml-2">
+        <i className="fa fa-save mr-2" />
+        {t('Lưu')}
+      </Button>
+      <Button color="primary" className="ml-2">
+        <i className="fa fa-send mr-2" />
+        {t('Nộp')}
+      </Button>
+    </>
+  );
+
+  const renderFilters = (): JSX.Element => (
+    <div className="bg-white p-3 shadow-sm">
+      <Row>
+        <Col xs={12} md={3} xl={2}>
+          <FormGroup>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+        </Col>
+        <Col xs={12} md={3} xl={2}>
+          <FormGroup>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+        </Col>
+      </Row>
+    </div>
+  );
+
+  const renderThongTinBangKe = (): JSX.Element => (
+    <div className="bg-white p-3 shadow-sm">
+      <Row>
+        <Col>
+          <div>{t('Mã bảng kê')}:</div>
+          <div>{t('Trạng thái')}:</div>
+          <div>{t('Kỳ')}:</div>
+        </Col>
+        <Col>
+          <div>{t('Người tạo')}:</div>
+          <div>{t('Đơn vị')}:</div>
+        </Col>
+        <Col>
+          <div>{t('Tổng giá trị')}:</div>
+          <div>{t('Ngày tạo')}:</div>
+        </Col>
+      </Row>
+    </div>
+  );
+
+  const renderSecondControllers = (): JSX.Element => (
+    <>
+      <Button color="primary" className="ml-2">
+        <i className="fa fa-upload mr-2" />
+        {t('Tải lên')}
+      </Button>
+      <Button color="primary" className="ml-2">
+        <i className="fa fa-plus mr-2" />
+        {t('Thêm khoản mục')}
+      </Button>
+    </>
+  );
+
   return (
     <>
-      <Row className="mb-3 sipTitleContainer">
-        <h1 className="sipTitle">
-          <Button className="sipTitleBtnBack" onClick={handleBack}>
-            <img className="backIcon" src={'../../assets/img/icon/iconArrowLeft.svg'} alt="VTPostek" />
-          </Button>
-          {t('Tạo mới bảng kê')}
-        </h1>
-        <div className="sipTitleRightBlock">{renderTopController()}</div>
+      <Row className="mb-3">
+        <Col>
+          <h1 className="sipTitle">
+            <Button className="sipTitleBtnBack" onClick={handleBack}>
+              <img className="backIcon" src={'../../assets/img/icon/iconArrowLeft.svg'} alt="VTPostek" />
+            </Button>
+            {t('Tạo mới bảng kê')}
+          </h1>
+        </Col>
+        <Col className="d-flex justify-content-end">{renderFirstControllers()}</Col>
       </Row>
 
-      <Row className="sipBgWhiteContainer">
-        <div className="sipScanCodeContainer">
-          <Input type="text" placeholder="Quét mã bưu gửi" />
-          <Button color="primary">Quét mã</Button>
-        </div>
+      <Row className="mb-3">
+        <Col>{renderFilters()}</Col>
       </Row>
 
-      <Row className="sipTableContainer">
-        <DataTable columns={columns} data={[]} />
+      <Row className="mb-4">
+        <Col>{renderThongTinBangKe()}</Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Col>
+          <h1 className="sipTitle">{t('Danh sách khoản mục chi phí')}</h1>
+        </Col>
+        <Col className="d-flex justify-content-end">{renderSecondControllers()}</Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <div className="sipTableContainer">
+            <DataTable columns={columns} data={[]} />
+          </div>
+        </Col>
       </Row>
     </>
   );
