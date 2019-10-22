d6561a8ef30c4d81f79e960464a12fe6cc0e4eec
nguyen manh luan <manhluanvt90@gmail.com>
2019-10-22 11:14:31 +0700

    feat: 🎸 Filter for DanhSachBangKe Fico

--- src/containers/KeKhaiChiPhi/DanhSachBangKe.tsx
+++ src/containers/KeKhaiChiPhi/DanhSachBangKe.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { match } from 'react-router-dom';
 import { push } from 'connected-react-router';
-import { get } from 'lodash';
+import { get, map } from 'lodash';
 import { Button, Col, Input, Row, Label } from 'reactstrap';
 import { Cell } from 'react-table';
 import DateRangePicker from 'react-bootstrap-daterangepicker';
@@ -11,55 +11,79 @@ import 'bootstrap-daterangepicker/daterangepicker.css';
 import moment from 'moment';
 
 import { action_ZFI002 } from 'redux/ZFI002/actions';
-import { select_ZFI002 } from 'redux/ZFI002/selectors';
+import { select_ZFI002, select_ZFI002Count } from 'redux/ZFI002/selectors';
 import DataTable from 'components/DataTable';
-import ButtonPrintable from 'components/Button/ButtonPrintable';
-import PrintBangKeChiTiet from 'components/Printable/PrintBangKeChiTiet';
 import routesMap from 'utils/routesMap';
+import { makeSelectorMaBP } from 'redux/auth/selectors';
+import Pagination from '../../components/Pagination';
 
 interface Props {
   match: match;
 }
 
-const stateMap = {
-  A: 'Chờ phê duyệt',
-  B: 'Tạo mới',
-  C: 'Duyệt 1 phần',
-  D: 'Phê duyệt',
-};
+const stateMap = ['Tạo mới', 'Chờ phê duyệt', 'Phê duyệt', 'Duyệt 1 phần'];
 
 // eslint-disable-next-line max-lines-per-function
 const DanhSachBangKe = (props: Props): JSX.Element => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-
+  const userMaBp = useSelector(makeSelectorMaBP);
   const dataTable = useSelector(select_ZFI002);
-  const [tuKy, setTuKy] = useState<string>('01/01/2019');
-  const [denKy, setDenKy] = useState<string>('01/01/2020');
-  const [filterTimeValue, setFilterTimeValue] = useState<string>('');
+  const totalPage = useSelector(select_ZFI002Count);
 
-  React.useEffect((): void => {
+  const [tuKy, setTuKy] = useState<string>('');
+  const [denKy, setDenKy] = useState<string>('');
+  const [filterTimeValue, setFilterTimeValue] = useState<string>('');
+  const [idSearch, setIdSearch] = useState<string>('');
+  const [typeSearch, setTypeSearch] = useState<string>('');
+
+  const getListBangKe = (payload = {}): void => {
     const payloadListBk = {
-      TU_KY: '201901',
-      DEN_KY: '201910',
-      MA_BUU_CUC: 'BDH',
+      TU_KY: moment().format('YYYYMM'),
+      DEN_KY: moment().format('YYYYMM'),
+      MA_BUU_CUC: userMaBp,
       BK_ID: '',
       BK_STATUS: '2',
       IV_PAGENO: '1',
       IV_NO_PER_PAGE: '10',
+      ...payload,
     };
     dispatch(action_ZFI002(payloadListBk));
+  };
+
+  function handleSearchBangKe(): void {
+    getListBangKe({ BK_ID: idSearch, TU_KY: tuKy, DEN_KY: denKy, BK_STATUS: typeSearch });
+  }
+
+  const onPaginationChange = (selectedItem: { selected: number }): void => {
+    getListBangKe({
+      BK_ID: idSearch,
+      TU_KY: tuKy,
+      DEN_KY: denKy,
+      BK_STATUS: typeSearch,
+      IV_PAGENO: selectedItem.selected + 1,
+    });
+  };
+
+  React.useEffect((): void => {
+    getListBangKe();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [userMaBp]);
+
+  function handleChangeTextboxValue(setValueFunction: Function): (event: React.FormEvent<HTMLInputElement>) => void {
+    return (event: React.FormEvent<HTMLInputElement>): void => {
+      setValueFunction(event.currentTarget.value);
+    };
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function handleChangeDeliveryTime(event: any, picker: any): void {
     if (tuKy && denKy) {
-      const thisTuKy = moment(get(picker, 'startDate')).format('DD/MM/YYYY');
-      const thisDenKy = moment(get(picker, 'endDate')).format('DD/MM/YYYY');
-      setTuKy(thisTuKy);
-      setDenKy(thisDenKy);
-      setFilterTimeValue(`${thisTuKy} - ${thisDenKy}`);
+      setTuKy(moment(get(picker, 'startDate')).format('YYYYMM'));
+      setDenKy(moment(get(picker, 'endDate')).format('YYYYMM'));
+      setFilterTimeValue(
+        `${moment(get(picker, 'startDate')).format('MM/YYYY')} - ${moment(get(picker, 'endDate')).format('MM/YYYY')}`,
+      );
     }
   }
 
@@ -89,64 +113,6 @@ const DanhSachBangKe = (props: Props): JSX.Element => {
     );
   };
 
-  const renderPrintButton = (idChuyenThu: string): JSX.Element => (
-    <ButtonPrintable
-      btnProps={{
-        className: 'SipTableFunctionIcon',
-        children: <img src={'../../assets/img/icon/iconPrint.svg'} alt="VTPostek" />,
-      }}
-      modalBodyProps={{
-        children: <PrintBangKeChiTiet idChuyenThu={idChuyenThu} />,
-      }}
-      modalHeaderProps={{
-        children: t('In danh sách bưu gửi của bảng kê'),
-      }}
-    />
-  );
-
-  // const data = [
-  //   {
-  //     TYPE1: 'a',
-  //     TYPE2: 'b',
-  //     TYPE3: 'c',
-  //     TYPE4: 'd',
-  //     TYPE5: 'A',
-  //     TYPE6: 'f',
-  //   },
-  //   {
-  //     TYPE1: 'a',
-  //     TYPE2: 'b',
-  //     TYPE3: 'c',
-  //     TYPE4: 'd',
-  //     TYPE5: 'B',
-  //     TYPE6: 'f',
-  //   },
-  //   {
-  //     TYPE1: 'a',
-  //     TYPE2: 'b',
-  //     TYPE3: 'c',
-  //     TYPE4: 'd',
-  //     TYPE5: 'C',
-  //     TYPE6: 'f',
-  //   },
-  //   {
-  //     TYPE1: 'a',
-  //     TYPE2: 'b',
-  //     TYPE3: 'c',
-  //     TYPE4: 'd',
-  //     TYPE5: 'D',
-  //     TYPE6: 'f',
-  //   },
-  //   {
-  //     TYPE1: 'a',
-  //     TYPE2: 'b',
-  //     TYPE3: 'c',
-  //     TYPE4: 'd',
-  //     TYPE5: 'A',
-  //     TYPE6: 'f',
-  //   },
-  // ];
-
   const columns = useMemo(
     //eslint-disable-next-line max-lines-per-function
     () => [
@@ -171,44 +137,62 @@ const DanhSachBangKe = (props: Props): JSX.Element => {
       {
         Header: t('Ngày tạo'),
         accessor: 'CRE_TIME',
+        Cell: ({ row }: Cell<API.Child>): JSX.Element => {
+          const thisDate = moment(get(row, 'values.CRE_TIME'), 'YYYYMMDDHHmmss').format('DD/MM/YYYY');
+          return <>{thisDate}</>;
+        },
       },
       {
         Header: t('Kỳ'),
         accessor: 'BK_MONTH',
+        Cell: ({ row }: Cell<API.Child>): JSX.Element => {
+          const thisMonth = get(row, 'values.BK_MONTH');
+          const thisYear = get(row, 'values.BK_YEAR');
+          return (
+            <>
+              {thisMonth}/{thisYear}
+            </>
+          );
+        },
       },
       {
         Header: t('Trạng thái'),
         accessor: 'BK_STATUS',
         Cell: ({ row }: Cell<API.Child>): JSX.Element => {
-          const type5 = get(row, 'values.TYPE5');
-          const type5Text = get(stateMap, type5);
+          const thisStatus = get(row, 'values.BK_STATUS', 0);
+          const statusText = get(stateMap, `[${thisStatus}]`);
           return (
             <>
-              <Button className={`sipTableBtnStatus sipTableBtnStatus${type5}`}>{type5Text}</Button>
+              <Button className={`sipTableBtnStatus sipTableBtnStatus${thisStatus}`}>{statusText}</Button>
             </>
           );
         },
       },
       {
-        Header: t('Người duyệt'),
-        accessor: 'BK_ID',
+        Header: t('Người cập nhật'),
+        accessor: 'UDP_BY',
+        Cell: ({ row }: Cell<API.Child>): JSX.Element => {
+          return <>Chưa có mô tả</>;
+        },
       },
       {
         Header: t('Quản trị'),
         Cell: ({ row }: Cell<API.Child>): JSX.Element => {
-          return (
-            <>
-              {renderPrintButton('')}
-              <Button className="SipTableFunctionIcon">
-                <img src={'../../assets/img/icon/iconRemove.svg'} alt="VTPostek" />
-              </Button>
-            </>
+          const thisStatus = get(row, 'values.BK_STATUS', 0);
+          return thisStatus === 0 ? (
+            <Button className="SipTableFunctionIcon">
+              <img src={'../../assets/img/icon/iconRemove.svg'} alt="VTPostek" />
+            </Button>
+          ) : (
+            <Button className="SipTableFunctionIcon">
+              <img src={'../../assets/img/icon/iconEyes.svg'} alt="VTPostek" />
+            </Button>
           );
         },
       },
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
+    [dataTable],
   );
 
   return (
@@ -221,36 +205,49 @@ const DanhSachBangKe = (props: Props): JSX.Element => {
       <Row className="sipBgWhiteContainer sipFilterContainer">
         <Col className="sipFilterCol">
           <div className="sipFilterColSearch">
-            <Input type="text" placeholder="Tra cứu bảng kê" />
+            <Input
+              type="text"
+              placeholder="Tra cứu bảng kê"
+              onChange={handleChangeTextboxValue(setIdSearch)}
+              value={idSearch}
+            />
             <img src={'../../assets/img/icon/iconSearch.svg'} alt="VTPostek" />
           </div>
         </Col>
         <Col className="sipFilterCol">
-          <DateRangePicker
-            startDate={moment().format('MMDDYYYY')}
-            endDate="01012020"
-            onApply={handleChangeDeliveryTime}
-          >
+          <DateRangePicker onApply={handleChangeDeliveryTime}>
             <Input value={filterTimeValue} type="text" placeholder="Nhập khoảng thời gian" />
           </DateRangePicker>
         </Col>
         <Col className="sipFilterCol">
           <div className="sipFilterColSearch">
-            <Input type="select">
-              <option value={0}>Tất cả trạng thái</option>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
+            <Input type="select" onChange={handleChangeTextboxValue(setTypeSearch)} value={typeSearch}>
+              <option value="">Tất cả trạng thái</option>
+              {map(
+                stateMap,
+                (item: string, index: number): JSX.Element => {
+                  return <option value={index}>{item}</option>;
+                },
+              )}
             </Input>
             <img src={'../../assets/img/icon/iconFilter.svg'} alt="VTPostek" />
           </div>
         </Col>
         <Col className="sipFilterColBtn">
-          <Button color="primary">Tìm kiếm</Button>
+          <Button color="primary" onClick={handleSearchBangKe}>
+            Tìm kiếm
+          </Button>
         </Col>
       </Row>
 
       <Row className="sipTableContainer">
         <DataTable columns={columns} data={dataTable} />
+        <Pagination
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={2}
+          pageCount={totalPage}
+          onThisPaginationChange={onPaginationChange}
+        />
       </Row>
     </>
   );
--- src/redux/ZFI002/selectors.ts
+++ src/redux/ZFI002/selectors.ts
@@ -2,5 +2,10 @@ import { AppStateType } from 'redux/store';
 import { get } from 'lodash';
 
 export const select_ZFI002 = (state: AppStateType): API.ListMTBKRECEIVER[] => {
-  return get(state, 'ZFI002.response.MT_BK_RECEIVER.List', []);
+  const thisList = get(state, 'ZFI002.response.MT_BK_RECEIVER.List');
+  return thisList ? thisList : [];
+};
+
+export const select_ZFI002Count = (state: AppStateType): number => {
+  return parseInt(get(state, 'ZFI002.response.MT_BK_RECEIVER.Paging.EV_TOTAL_PAGE', 0));
 };
