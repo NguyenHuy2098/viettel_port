da13ab5f8ea14f2eee4a9c2dd04c5ab62e723891
giangdv <doanvangiangnd@gmail.com>
2019-11-02 16:41:44 +0700

    fix: 🐛 fix bug 1844 - them khoan muc in bang ke chi tiet

--- src/containers/KeKhaiChiPhi/PrintableBangKe.tsx
+++ src/containers/KeKhaiChiPhi/PrintableBangKe.tsx
@@ -291,7 +291,7 @@ const PrintableBangKe = (props: Props): JSX.Element => {
   }
 
   const renderGroupedRow = (rows: TableRow<API.LISTMTDETAILRECEIVER>[], index: string): JSX.Element => {
-    const row = rows.filter(item => get(item, 'original.KHOAN_MUC', '') === index)[0];
+    const row = rows.filter(item => get(item, 'original.KHOAN_MUC') === index)[0];
     const ten_km = get(row, 'original.TEN_KM', '');
     if (index === 'null') return <></>;
     return <div>{`${index} - ${ten_km}`}</div>;
