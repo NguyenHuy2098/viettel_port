bff88f2b0d6447209c3743903712c81436cdad06
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2020-01-03 16:55:10 +0700

    fix: 🐛 able to use commit hash to display as version

--- .env-cmdrc.js
+++ .env-cmdrc.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 const defaultEnv = {
   REACT_APP_BUILD_TIME: moment().format(),
-  REACT_APP_VERSION: process.env.CI_COMMIT_REF_NAME || process.env.CI_COMMIT_SHORT_SHA || '-',
+  REACT_APP_VERSION: process.env.CI_COMMIT_SHORT_SHA || '-',
 };
 
 module.exports = {
@@ -18,5 +18,6 @@ module.exports = {
     ...defaultEnv,
     GENERATE_SOURCEMAP: 'false',
     REACT_APP_ENV: 'production',
+    REACT_APP_VERSION: process.env.CI_COMMIT_REF_NAME,
   },
 };
